97a4a139359653a6bd0d1ed8a26501463f7333df
Andrew Kula <arkula931@gmail.com>
2024-09-26 13:00:02 -0500

    Finished basic logic of game

--- WarUpgradeGame.py
+++ WarUpgradeGame.py
@@ -44,29 +44,56 @@ while True:
     if (quit == True):
         break
 
-    """Generate next card"""
-    bot_index = random.randint(0,(len(cpu_cards) - 1))
-    bot_card = cpu_cards[bot_index]
-    my_index = random.randint(0,(len(user_cards) - 1))
-    my_card = user_cards[my_index]
-
-    """Cards played"""
-    print(f'\nBot plays: {bot_card}')
-    print(f'You play: {my_card}')
-
     """Find winner and alter collections"""
+    extraPile = []
+    while True:
+        """Generate Card"""
+        bot_index = random.randint(0,(len(cpu_cards) - 1))
+        bot_card = cpu_cards[bot_index]
+        my_index = random.randint(0,(len(user_cards) - 1))
+        my_card = user_cards[my_index]
 
-    bot_value_list = bot_card.split(' ')
-    bot_value = bot_value_list[0]
-    user_value_list = my_card.split(' ')
-    user_value = user_value_list[0]
-    if (RANK_VALUES[bot_value] > RANK_VALUES[user_value]):
-        user_cards.remove(my_card)
-        bot_pile.append(my_card)
-    elif (RANK_VALUES[bot_value] < RANK_VALUES[user_value]):
-        cpu_cards.remove(bot_card)
-        user_pile.append(bot_card)
+        """Cards played"""
+        print(f'\nBot plays: {bot_card}')
+        print(f'You play: {my_card}')
 
+        """Find Value"""
+        bot_value_list = bot_card.split(' ')
+        bot_value = bot_value_list[0]
+        user_value_list = my_card.split(' ')
+        user_value = user_value_list[0]
+
+        """Run Value operant"""
+        if (RANK_VALUES[bot_value] > RANK_VALUES[user_value]):
+            user_cards.remove(my_card)
+            bot_pile.append(my_card)
+            if (len(extraPile) > 0):
+                for i in range(len(extraPile)):
+                    bot_pile.append(extraPile[i])
+            break
+        elif (RANK_VALUES[bot_value] < RANK_VALUES[user_value]):
+            cpu_cards.remove(bot_card)
+            user_pile.append(bot_card)
+            if (len(extraPile) > 0):
+                for i in range(len(extraPile)):
+                    user_pile.append(extraPile[i])
+            break
+        elif (RANK_VALUES[bot_value] == RANK_VALUES[user_value]):
+            print('War!!! \nSetting down three cards')
+            cpu_cards.remove(bot_card)
+            user_cards.remove(my_card)
+            extraPile.append(my_card)
+            extraPile.append(bot_card)
+            for i in range(3):
+                bot_index = random.randint(0,(len(cpu_cards) - 1))
+                bot_card = cpu_cards[bot_index]
+                cpu_cards.remove(bot_card)
+                my_index = random.randint(0,(len(user_cards) - 1))
+                my_card = user_cards[my_index]
+                user_cards.remove(my_card)
+                extraPile.append(bot_card)
+                extraPile.append(my_card)
+            
     """Print out scores"""
     print()
     print(f'Bot cards:  {(len(cpu_cards) + len(bot_pile))}')
@@ -80,6 +107,17 @@ while True:
         cpu_cards = bot_pile
         bot_pile = []
 
+    """Check for winner"""
+    if ((user_cards == [] and user_pile == [])):
+        print('Bot wins!')
+        quit = True
+        break
+    if ((cpu_cards == [] and bot_pile == [])):
+        print('You win!')
+        quit = True
+        break
+
+    """Next Card"""
     while True:
         print('Next Card? (y/n)')
         response = input()
@@ -99,10 +137,3 @@ while True:
         else:
             print('Invalid input')
             continue
-
-    if ((user_cards == [] and user_pile == [])):
-        print('Bot wins!')
-        quit = True
-    if ((cpu_cards == [] and bot_pile == [])):
-        print('You win!')
-        quit = True
