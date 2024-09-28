38f3324108008080d76bc07199a7f0c4ca2724f9
Andrew Kula <arkula931@gmail.com>
2024-09-28 09:25:13 -0500

    Reorganizing/Pygame Work

---
+++ PyGame/PygameTest.py
@@ -0,0 +1,44 @@
+import pygame
+
+# pygame setup
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.clock = pygame.time.Clock()
+        self.dt = 0
+
+        pygame.display.set_caption('Starter Game')
+        self.player_pos = pygame.Vector2(self.screen.get_width() / 2, self.screen.get_height() / 2)
+
+    def run(self):
+        while True:
+            # pygame.QUIT event means the user clicked X to close your window
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+            # fill the screen with a color to wipe away anything from last frame
+            self.screen.fill("purple")
+
+            pygame.draw.circle(self.screen, "red", self.player_pos, 40)
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                self.player_pos.y -= 300 * dt
+            if keys[pygame.K_s]:
+                self.player_pos.y += 300 * dt
+            if keys[pygame.K_a]:
+                self.player_pos.x -= 300 * dt
+            if keys[pygame.K_d]:
+                self.player_pos.x += 300 * dt
+
+            # flip() the display to put your work on screen
+            pygame.display.flip()
+
+            # limits FPS to 60
+            # dt is delta time in seconds since last frame, used for framerate-
+            # independent physics.
+            dt = self.clock.tick(60) / 1000
+
+Game().run()
--- Random.py
+++
@@ -1,9 +0,0 @@
-import random
-
-hours = random.randint(0,23)
-minutes = random.randint(0,59)
-
-if (minutes < 10):
-    minutes = '0' + str(minutes)
-    
-print(str(hours) + ':' + str(minutes))
